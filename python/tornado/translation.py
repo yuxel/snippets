9b5c557477f93a479a2129db8cac1bf642f72cb5
Osman Yuksel <yuxel@sonsuzdongu.com>
2010-08-07 21:15:36 +0300

    translation example changed

--- python/tornado/translation.py
+++ python/tornado/translation.py
@@ -8,22 +8,29 @@ import tornado.locale
 import os
 
 # handles main page 
-class MainHandler(tornado.web.RequestHandler):
+class TRHandler(tornado.web.RequestHandler):
     def get(self):
+        tornado.locale.set_default_locale('tr_TR')
         self.render("templates/translation.html")
 
+class ENHandler(tornado.web.RequestHandler):
+    def get(self):
+        tornado.locale.set_default_locale('en_US')
+        self.render("templates/translation.html")
+
+
 application = tornado.web.Application([
-    (r"/", MainHandler),
+    (r"/tr/", TRHandler),
+    (r"/en/", ENHandler),
 ])
 
 if __name__ == "__main__":
-
-    #tornado.locale.set_default_locale('tr_TR')
-    
     #set path for location dir
-    tornado.locale.load_translations(os.path.join(os.path.dirname(__file__),                                                                                       "translations"))
-
+ 
+    translationsPath = os.path.join(os.path.dirname(__file__), "translations")  
+    tornado.locale.load_translations(translationsPath)
 
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(8888)
-tornado.ioloop.IOLoop.instance().start()
+
+    tornado.ioloop.IOLoop.instance().start()
